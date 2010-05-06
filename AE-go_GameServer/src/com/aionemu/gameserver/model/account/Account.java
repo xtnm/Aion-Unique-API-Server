9f063699defb0a5e01765550b5e58830e2991f01
ATracer <ATracer@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-05-06 19:13:40 +0000

    fix for character creation error

--- AE-go_GameServer/src/com/aionemu/gameserver/model/account/Account.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/model/account/Account.java
@@ -17,6 +17,7 @@
 
 package com.aionemu.gameserver.model.account;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -195,6 +196,12 @@ public class Account implements Iterable<PlayerAccountData>
 			@Override
 			public int compare(PlayerAccountData x, PlayerAccountData y)
 			{
+				Timestamp t1 = x.getPlayerCommonData().getLastOnline();
+				Timestamp t2 = x.getPlayerCommonData().getLastOnline();
+				if(t2 == null)
+					return 1;
+				else if(t1 == null)
+					return -1;
 				return y.getPlayerCommonData().getLastOnline().compareTo(x.getPlayerCommonData().getLastOnline());
 			}
 		});
