b590550c040d0ae4676e1cd5956f71070a6e6138
alikasm8 <alikasm8@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-05-09 08:49:22 +0000

    My Bad, forgot to add script.

---
+++ AE-go_GameServer/data/scripts/system/handlers/quest/morheim/_2034TheHandBehindtheIceClaw.java
@@ -0,0 +1,220 @@
+/*
+ * This file is part of aion-unique <aion-unique.org>.
+ *
+ * aion-unique is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aion-unique is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with aion-unique.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package quest.morheim;
+
+import java.util.Collections;
+
+import com.aionemu.gameserver.model.gameobjects.Npc;
+import com.aionemu.gameserver.model.gameobjects.player.Player;
+import com.aionemu.gameserver.model.templates.quest.QuestItems;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_DIALOG_WINDOW;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_EMOTION;
+import com.aionemu.gameserver.network.aion.serverpackets.SM_USE_OBJECT;
+import com.aionemu.gameserver.questEngine.handlers.QuestHandler;
+import com.aionemu.gameserver.questEngine.model.QuestEnv;
+import com.aionemu.gameserver.questEngine.model.QuestState;
+import com.aionemu.gameserver.questEngine.model.QuestStatus;
+import com.aionemu.gameserver.services.ItemService;
+import com.aionemu.gameserver.utils.PacketSendUtility;
+import com.aionemu.gameserver.utils.ThreadPoolManager;
+import com.google.inject.Inject;
+
+/**
+ * @author Rhys2002
+ * 
+ */
+public class _2034TheHandBehindtheIceClaw extends QuestHandler
+{
+	@Inject
+	ItemService itemService;
+	
+	private final static int	questId	= 2034;
+	private final static int[]	npc_ids	= { 204303, 204332, 700246, 204301 };
+
+	public _2034TheHandBehindtheIceClaw()
+	{
+		super(questId);
+	}
+
+	@Override
+	public void register()
+	{
+		qe.addQuestLvlUp(questId);
+		qe.setNpcQuestData(204417).addOnKillEvent(questId);
+		qe.setNpcQuestData(212877).addOnKillEvent(questId);
+		for(int npc_id : npc_ids)
+			qe.setNpcQuestData(npc_id).addOnTalkEvent(questId);	 
+	}
+
+	@Override
+	public boolean onLvlUpEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null || qs.getStatus() != QuestStatus.LOCKED || player.getCommonData().getLevel() < 25)
+			return false;
+
+		QuestState qs2 = player.getQuestStateList().getQuestState(2300);
+		if(qs2 == null || qs2.getStatus() != QuestStatus.COMPLETE)
+			return false;
+		qs.setStatus(QuestStatus.START);
+		updateQuestStatus(player, qs);
+		return true;
+	}
+
+	@Override
+	public boolean onDialogEvent(QuestEnv env)
+	{
+		final Player player = env.getPlayer();
+		final int titleId = player.getCommonData().getTitleId();		
+		final QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null)
+			return false;
+
+		int var = qs.getQuestVarById(0);
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+
+		if(qs.getStatus() == QuestStatus.REWARD)
+		{
+			if(targetId == 204301)
+			{
+				if(env.getDialogId() == -1)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 10002);
+				else if(env.getDialogId() == 1009)
+					return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 5);
+				else return defaultQuestEndDialog(env);
+			}
+			return false;
+		}
+		else if(qs.getStatus() != QuestStatus.START)
+		{
+			return false;
+		}
+		if(targetId == 204303)
+		{
+			switch(env.getDialogId())
+			{
+				case 25:
+					if(var == 0)
+						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1011);
+					else if(var == 5)
+						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 2716);						
+				case 10000:
+					if(var == 0)
+					{
+						qs.setQuestVarById(0, var + 1);
+						updateQuestStatus(player, qs);
+						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+						return true;
+					}
+				case 10255:
+					if(var == 5)
+					{
+						qs.setStatus(QuestStatus.REWARD);					
+						updateQuestStatus(player, qs);
+						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+						return true;
+					}					
+			}
+		}
+		else if(targetId == 204332)
+		{
+			switch(env.getDialogId())
+			{
+				case 25:
+					if(var == 1)
+						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1352);
+					else if(var == 2)
+					{
+						if(player.getInventory().getItemCountByItemId(182204008) == 0)
+							return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1694);
+						else 
+							return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 1693);
+					}
+					else if(var == 3)
+						return sendQuestDialog(player, env.getVisibleObject().getObjectId(), 2034);						
+				case 10001:
+					if(var == 1)
+					{
+						qs.setQuestVarById(0, var + 1);
+						updateQuestStatus(player, qs);
+					}
+						itemService.addItems(player, Collections.singletonList(new QuestItems(182204008, 1)));
+						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+						return true;
+				case 10003:
+					if(var == 3)
+					{
+						qs.setQuestVarById(0, var + 1);
+						updateQuestStatus(player, qs);
+						PacketSendUtility.sendPacket(player, new SM_DIALOG_WINDOW(env.getVisibleObject().getObjectId(), 10));
+						return true;
+					}
+			}
+		}
+		else if(targetId == 700246 && player.getInventory().getItemCountByItemId(182204019) >= 1)
+		{
+			if(env.getDialogId() == -1 && var == 2)
+			{
+			final int targetObjectId = env.getVisibleObject().getObjectId();
+				PacketSendUtility.sendPacket(player, new SM_USE_OBJECT(player.getObjectId(), targetObjectId, 3000, 1));
+				PacketSendUtility.broadcastPacket(player, new SM_EMOTION(player, 37, 0, targetObjectId), true);
+				ThreadPoolManager.getInstance().schedule(new Runnable(){
+					@Override
+					public void run()
+					{
+						PacketSendUtility.sendPacket(player, new SM_USE_OBJECT(player.getObjectId(), targetObjectId, 3000, 0));
+						PacketSendUtility.broadcastPacket(player, new SM_EMOTION(player, 38, 0, targetObjectId), true);
+						questService.addNewSpawn(220020000, 1, 204417, (float) 461.23,
+														(float) 188.82, (float) 485.61, (byte) 25, true);
+						player.getInventory().removeFromBagByItemId(182204008, 1);								
+						player.getInventory().removeFromBagByItemId(182204019, 1);
+					}
+				}, 3000);
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean onKillEvent(QuestEnv env)
+	{
+		Player player = env.getPlayer();
+		QuestState qs = player.getQuestStateList().getQuestState(questId);
+		if(qs == null || qs.getStatus() != QuestStatus.START)
+			return false;
+			
+		int targetId = 0;
+		if(env.getVisibleObject() instanceof Npc)
+			targetId = ((Npc) env.getVisibleObject()).getNpcId();
+		
+		if(targetId == 204417 && qs.getQuestVarById(0) == 2)
+		{
+			player.getTitleList().addTitle(58);
+			qs.setQuestVarById(0, 3);
+			updateQuestStatus(player, qs);
+		}
+		if(targetId == 212877 && qs.getQuestVarById(0) == 4)
+		{
+			qs.setQuestVarById(0, 5);
+			updateQuestStatus(player, qs);
+		}		
+		return false;
+	}	
+}
