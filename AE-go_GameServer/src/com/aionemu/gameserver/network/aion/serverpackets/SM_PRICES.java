521559f62c31c4a259ab8c74b0e99126bcab31bc
Sarynth <Sarynth@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-06-11 19:43:04 +0000

    Fix for previous SM_PRICES commit. Buy price should be correct now. Displayed sell price still 2.5x above actual.

--- AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_PRICES.java
+++ AE-go_GameServer/src/com/aionemu/gameserver/network/aion/serverpackets/SM_PRICES.java
@@ -22,7 +22,7 @@ import com.aionemu.gameserver.network.aion.AionConnection;
 import com.aionemu.gameserver.network.aion.AionServerPacket;
 
 /**
- * @author xavier, Rama
+ * @author xavier
  * Price/tax in Influence ration dialog
  */
 public class SM_PRICES extends AionServerPacket
@@ -34,8 +34,8 @@ public class SM_PRICES extends AionServerPacket
 	@Override
 	protected void writeImpl(AionConnection con, ByteBuffer buf)
 	{
-		writeC(buf, 50); //Buying Price %
-        writeC(buf, 10); //Selling Price %
-        writeC(buf, 100); //Tax = -100 + C %
+		writeC(buf, 100); // Display Buying Price %
+        writeC(buf, 50);  // Buying Modified Price %
+        writeC(buf, 100); // Tax = -100 + C %
 	}
 }
