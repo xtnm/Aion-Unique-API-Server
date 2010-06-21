eb7495c0d4b02a9d80653de24cd9531ce55ca65a
zdead <zdead@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-06-21 18:36:40 +0000

    [trunk] Added StopLS.sh to stop LoginServer under Unix systems.

--- AE-go_LoginServer/dist/StartLS.sh
+++ AE-go_LoginServer/dist/StartLS.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 java -Xms8m -Xmx32m -ea -Xbootclasspath/p:./libs/jsr166.jar -javaagent:libs/ae_commons.jar -cp ./libs/*:ae_login.jar com.aionemu.loginserver.LoginServer
+lspid=$!
+echo ${lspid} > loginserver.pid
+echo "LoginServer started!"
