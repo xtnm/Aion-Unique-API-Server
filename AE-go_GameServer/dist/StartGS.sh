8e3af148baf3a1ab377e189fcf6802d751ca36be
zdead <zdead@a7ae0448-8ed6-4276-8d1f-9f05efaf5f4a>
2010-06-23 15:42:01 +0000

    [trunk] StartGS.sh update : server can now be started without restarter loop under Unix, using:
    # ./StartGS.sh noloop

--- AE-go_GameServer/dist/StartGS.sh
+++ AE-go_GameServer/dist/StartGS.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-./StartGS_loop.sh &
+case $1 in
+noloop)
+  [ -d log/ ] || mkdir log/
+  [ -f log/console.log ] && mv log/console.log "log/console/`date +%Y-%m-%d_%H-%M-%S`_console.log"
+  java -Xms128m -Xmx1536m -ea -Xbootclasspath/p:./libs/jsr166.jar -javaagent:libs/ae_commons.jar -cp ./libs/*:ae_gameserver.jar com.aionemu.gameserver.GameServer > log/console.log 2>&1
+  echo $! > gameserver.pid
+  echo "Server started!"
+  ;;
+*)
+  ./StartGS_loop.sh &
+  ;;
+esac
